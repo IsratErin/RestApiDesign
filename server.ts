25a1fb09554a405901192c6fb27276d1f0994377
IsratErin <131592678+IsratErin@users.noreply.github.com>
2025-09-29 20:14:26 +0200

    pastry interface defined

--- server.ts
+++ server.ts
@@ -4,8 +4,39 @@ const app = express();
 
 const PORT = 3000;
 
-app.use(express.json());
+//app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+interface Pastry {
+  id: number;
+  flavour: string;
+  name: string;
+}
+
+const pastries: Pastry[] = [
+  {
+    id: 1,
+    flavour: "chocolate",
+    name: "chocolate brownie",
+  },
+  {
+    id: 3,
+    flavour: "vanilla",
+    name: "vanilla brownie",
+  },
+  {
+    id: 3,
+    flavour: "strawberry",
+    name: "strawberry cheesecake",
+  },
+];
+
+app.get("/", (req, res) => {
+  const bakerry = {
+    pastries: "wee",
+  };
+  res.json(bakerry);
 });
